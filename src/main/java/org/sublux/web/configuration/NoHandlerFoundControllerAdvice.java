a2731b7f4f4ad3f11c9897b4854dceed0faaf05d
MasterLogick <31408232+masterlogick@users.noreply.github.com>
2021-10-18 17:12:24 +0300

    fix validation exception response

--- src/main/java/org/sublux/web/configuration/NoHandlerFoundControllerAdvice.java
+++ src/main/java/org/sublux/web/configuration/NoHandlerFoundControllerAdvice.java
@@ -1,31 +1,26 @@
 package org.sublux.web.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.NoHandlerFoundException;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 @ControllerAdvice
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class NoHandlerFoundControllerAdvice extends ResponseEntityExceptionHandler {
+public class NoHandlerFoundControllerAdvice {
     @Value("classpath:/static/index.html")
     private Resource index;
     private String data = null;
 
-    @Override
-    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex) {
         if (data == null) {
             try {
                 data = FileCopyUtils.copyToString(new InputStreamReader(index.getInputStream()));
--- src/main/java/org/sublux/web/configuration/ValidationErrorHandlingControllerAdvice.java
+++ src/main/java/org/sublux/web/configuration/ValidationErrorHandlingControllerAdvice.java
@@ -1,24 +1,22 @@
 package org.sublux.web.configuration;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.sublux.web.controller.ValidationErrorResponse;
 import org.sublux.web.form.ValidationError;
 
 import java.util.HashSet;
 
 @ControllerAdvice
-public class ValidationErrorHandlingControllerAdvice extends ResponseEntityExceptionHandler {
+public class ValidationErrorHandlingControllerAdvice {
 
-    @Override
-    protected ResponseEntity<Object> handleBindException(BindException e, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<Object> handleBindException(BindException e) {
         ValidationErrorResponse ver = new ValidationErrorResponse(new HashSet<>());
-        e.getAllErrors().forEach(err -> ver.getErrorList().add(new ValidationError(err.getDefaultMessage(), err.getObjectName())));
+        e.getFieldErrors().forEach(err -> ver.getErrorList().add(new ValidationError(err.getDefaultMessage(), err.getField())));
+        e.getGlobalErrors().forEach(err -> ver.getErrorList().add(new ValidationError(err.getDefaultMessage(), "GLOBAL")));
         return ResponseEntity.badRequest().body(ver);
     }
 }
